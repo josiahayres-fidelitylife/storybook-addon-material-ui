b13bf8e8b6b49df4492a77a32b8a81e50a37b47b
josiahayres-fidelitylife <105252820+josiahayres-fidelitylife@users.noreply.github.com>
2022-06-14 20:07:53 +1200

    Update muiTheme.js

--- src/muiTheme.js
+++ src/muiTheme.js
@@ -20,7 +20,7 @@ const darkBaseTheme = createTheme({
 lightBaseTheme.themeName = 'Light Theme';
 darkBaseTheme.themeName = 'Dark Theme';
 
-export function muiTheme(themes) {
+export function muiTheme(themes, config = {withCssBaseline: false}) {
   const store = createStore(
     EVENT_ID_INIT,
     EVENT_ID_DATA,
@@ -60,7 +60,7 @@ export function muiTheme(themes) {
     return (
       <MuiDecorator
         story={storyItem}
-        initData={{ themes: themesInitList, themeInd: 0 }}
+        initData={{ themes: themesInitList, themeInd: 0, withCssBaseline: config.withCssBaseline }}
       />
     );
   };
