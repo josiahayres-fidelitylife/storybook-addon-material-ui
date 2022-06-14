a48e4389fb8d44103af3266bceaa1ea354818db6
josiahayres-fidelitylife <105252820+josiahayres-fidelitylife@users.noreply.github.com>
2022-06-14 19:57:34 +1200

    Working for React 18, Storybook 6 and MUI 5

--- src/UI/MuiDecorator.js
+++ src/UI/MuiDecorator.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { ThemeProvider, StyledEngineProvider, createTheme } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+import { ThemeProvider as Emotion10ThemeProvider } from 'emotion-theming';
 
 import withChannel from '../adk/WithChannel';
 import { EVENT_ID_INIT, EVENT_ID_DATA, EVENT_ID_BACK } from '../config';
@@ -15,8 +18,9 @@ const currentTheme = data => {
 
 // eslint-disable-next-line react/prop-types
 const MuiDecorator = ({ data, story }) => (
-  <StyledEngineProvider injectFirst>
+  <Emotion10ThemeProvider injectFirst theme={currentTheme(data)}>
     <ThemeProvider theme={currentTheme(data)}>
+      <CssBaseline />
       <div>{story}</div>
     </ThemeProvider>
   </StyledEngineProvider>
